c66cce6f00e51f88593cc2ad8755625a79db0258
Mariano Molina <mariano@contenidos-digitales.com>
2017-10-18 18:36:21 -0300

    Add readme

--- app/routes/hotels_routes.js
+++ app/routes/hotels_routes.js
@@ -4,7 +4,6 @@ module.exports = (app, db) => {
         res.send('Hoteles?');
     });
 
-    // https://almundo.com.ar/hotels/async/1324298/allresult?date=2017-11-06,2017-11-17&rooms=2&type=CITY&limit=20
     app.get('/hotels', (req, res) => {
         res.send({ 'hotels': [] });
     });
